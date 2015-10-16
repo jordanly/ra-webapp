159936a8ce96c056012b8352345970a05e607c36
Jennie Ju <jsj18@duke.edu>
2015-10-16 04:05:00 -0400

    Can load basic HTTP file from Spark framework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import freemarker.template.*;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        Configuration viewDir = new Configuration();
+        viewDir.setClassForTemplateLoading(Main.class, "/templates");
+
+        get("/hello", (req, res) -> {
+            return new ModelAndView(new HashMap<String, Object>(), "frontend.ftl");
+        }, new FreeMarkerEngine(viewDir));
     }
 }
 
